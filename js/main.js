87ded6266ead4f87d3824752255d04bc3b2849db
Ehmaed Alhams <ehmaedalzhar@gmail.com>
2023-01-01 19:11:40 +0200

    update: insert section our works.
    insert section our works, this section have galleries and insert some features.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector('.btn');
+let works = document.querySelector('.our-works');
+
+
+btn.onclick = function () {
+    // works.classList.toggle('container');
+    // if (works.className.includes = "container") {
+    //     btn.textContent = "Default";
+    // } else {
+    //     btn.textContent = "Full Screen";
+    // }
+
+    if (works.classList.contains('container')) {
+        btn.textContent = "Default";
+        works.classList.remove('container');
+    } else {
+        btn.textContent = "Full Screen";
+        works.classList.add('container');
+    }
+};
