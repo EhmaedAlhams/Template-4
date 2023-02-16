bf23763ac8ea68097e590db5971462ac855826d3
Ehmaed Alhams <ehmaedalzhar@gmail.com>
2023-02-16 20:32:43 +0200

    update: update section and insert another.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let btn = document.querySelector(".btn");
 let works = document.querySelector(".our-works");
+let lis = Array.from(document.querySelectorAll('.info-list > li'));
 
+// console.log(lis);
 btn.onclick = function () {
     // works.classList.toggle('container');
     // if (works.className.includes = "container") {
@@ -17,3 +19,46 @@ btn.onclick = function () {
         works.classList.add("container");
     }
 };
+
+
+// lis.addEventListener("click", () => {
+//     if (lis.classList.contains("selected")) {
+//         lis.classList.remove('selected');
+//     } else {
+//         lis.classList.add('selected');
+//     }
+//     console.log("select");
+// })
+
+// lis.onclick = () => {
+
+
+//     if (lis.classList.contains("selected")) {
+//         lis.classList.remove("selected");
+//     } else {
+//         lis.classList.add("selected");
+//     }
+
+// }
+
+
+
+// Solution 1:
+// Array.from(document.querySelectorAll('.info-list > li'), li => li.addEventListener("click", () => {
+//     if (li.classList.contains('selected')) {
+//         li.classList.remove('selected');
+//     } else {
+//         li.classList.add('selected');
+//     }
+// }));
+
+
+for (const li of document.querySelectorAll('.info-list > li')) {
+    li.addEventListener("click", () => {
+        if (li.classList.contains('.selected')) {
+            li.classList.add(".selected");
+        } else {
+            li.classList.remove('.selected');
+        }
+    });
+}
