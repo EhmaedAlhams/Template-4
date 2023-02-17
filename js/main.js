8b9360fa98d2410cb5d1f250096d843c76746c51
Ehmaed Alhams <ehmaedalzhar@gmail.com>
2023-02-17 21:58:21 +0200

    update: update solution.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 let btn = document.querySelector(".btn");
 let works = document.querySelector(".our-works");
-let lis = Array.from(document.querySelectorAll('.info-list > li'));
+let list = Array.from(document.querySelectorAll('.info-list > li'));
 
-// console.log(lis);
+
+// console.log(list);
 btn.onclick = function () {
     // works.classList.toggle('container');
     // if (works.className.includes = "container") {
@@ -44,21 +45,40 @@ btn.onclick = function () {
 
 
 // Solution 1:
-// Array.from(document.querySelectorAll('.info-list > li'), li => li.addEventListener("click", () => {
-//     if (li.classList.contains('selected')) {
-//         li.classList.remove('selected');
-//     } else {
-//         li.classList.add('selected');
-//     }
-// }));
+Array.from(document.querySelectorAll('.info-list > li'), li => li.addEventListener("click", function (event) {
+    if (event.target.classList.contains('selected')) {
+        li.classList.remove('selected');
+        li.className.indexOf(li);
+    } else {
+        li.classList.add('selected');
+    }
+}));
 
 
-for (const li of document.querySelectorAll('.info-list > li')) {
-    li.addEventListener("click", () => {
-        if (li.classList.contains('.selected')) {
-            li.classList.add(".selected");
-        } else {
-            li.classList.remove('.selected');
-        }
-    });
-}
+// Solution 2:
+// for (const li of document.querySelectorAll('.info-list > li')) {
+//     li.addEventListener("click", (event) => {
+//         // if (li.classList.contains('selected')) {
+//         //     li.classList.remove("selected");
+//         // } else {
+//         //     li.classList.add('selected');
+//         // }
+//         // console.log(li);
+//         if (event.target.classList.contains("selected")) {
+//             event.target.classList.remove("selected");
+//         } else {
+//             event.target.classList.add('selected');
+//         }
+//     });
+// }
+
+
+// list.forEach(li => {
+//     li.addEventListener("click", (list_item) => {
+//         if (list_item.target.classList.contains('selected')) {
+//             list_item.classList.remove('selected');
+//         } else {
+//             list_item.classList.add('selected');
+//         }
+//     });
+// });
